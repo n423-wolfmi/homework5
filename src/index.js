84ea745bf173aa0fef02e10c20b99a94df6999cf
wolfmatt233 <84036287+wolfmatt233@users.noreply.github.com>
2023-09-26 16:51:23 -0400

    genre filter + responsive style

--- src/index.js
+++ src/index.js
@@ -21,6 +21,16 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+var allBtn = document.getElementById("allBtn")
+var rockBtn = document.getElementById("rock")
+var metalBtn = document.getElementById("metal")
+var folkBtn = document.getElementById("folk")
+
+allBtn.addEventListener("click", getAlbums)
+rockBtn.addEventListener("click", filterGenre)
+metalBtn.addEventListener("click", filterGenre)
+folkBtn.addEventListener("click", filterGenre)
+
 async function getAlbums() {
   document.getElementById("display").innerHTML = "";
 
@@ -28,16 +38,41 @@ async function getAlbums() {
 
   querySnapshot.forEach((doc) => {
     document.getElementById("display").innerHTML += `
-        <div class="albumItem">
-          <img src="${doc.data().albumPhoto}" alt="${doc.data().albumPhoto}">
-          <div class="albumInfo">
-            <p><b>Album: </b>${doc.data().albumName}</p>
-            <p><b>Artist: </b>${doc.data().artistName}</p>
-            <p><b>Genre</b>: </b>${doc.data().genre}</p>
-          </div>
+      <div class="albumItem">
+        <img src="${doc.data().albumPhoto}" alt="${doc.data().albumPhoto}">
+        <div class="albumInfo">
+          <p><b>Album: </b>${doc.data().albumName}</p>
+          <p><b>Artist: </b>${doc.data().artistName}</p>
+          <p><b>Genre</b>: </b>${doc.data().genre}</p>
         </div>
-        
-      `;
+      </div>
+    `;
+  });
+};
+
+async function filterGenre(e) {
+  document.getElementById("display").innerHTML = "";
+
+  let genre = e.currentTarget.value
+
+  const q = query(
+    collection(db, "Albums"),
+    where("genre", "==", genre)
+  );
+
+  const querySnapshot = await getDocs(q);
+
+  querySnapshot.forEach((doc) => {
+    document.getElementById("display").innerHTML += `
+      <div class="albumItem">
+        <img src="${doc.data().albumPhoto}" alt="${doc.data().albumPhoto}">
+        <div class="albumInfo">
+          <p><b>Album: </b>${doc.data().albumName}</p>
+          <p><b>Artist: </b>${doc.data().artistName}</p>
+          <p><b>Genre</b>: </b>${doc.data().genre}</p>
+        </div>
+      </div>
+    `;
   });
 }
 
